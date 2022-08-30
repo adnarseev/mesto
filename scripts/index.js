277730c5e50003affefaeb174d96a2a374534852
Aleksey Narseev <adnarseev@yandex.ru>
2022-08-30 23:01:38 +0300

    Fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,29 @@
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
-let nameinput = document.querySelector('.popup__input_type_title');
-let Jobinput = document.querySelector('.popup__input_type_subtitle');
-function addPopup() {
-  popup.classList.add('popup_opened');
-  nameinput.value = profileTitle.textContent;
-  Jobinput.value = profileSubtitle.textContent;
-}
-editButton.addEventListener('click', addPopup);
-
+let nameInput = document.querySelector('.popup__input_type_title');
+let jobInput = document.querySelector('.popup__input_type_subtitle');
+let closeButton = document.querySelector('.popup__close-button')
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
-
 let form = document.querySelector('.popup__form');
+
+let removePopup = function () {
+  popup.classList.remove('popup_opened', 'popup-place_opened');
+}
+
+function addPopup() {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+}
 
 function rename(evt) {
   evt.preventDefault();
-  profileTitle.textContent = nameinput.value;
-  profileSubtitle.textContent = Jobinput.value;
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
   removePopup();
 }
 
 form.addEventListener('submit', rename);
-
-let closeButton = document.querySelector('.popup__close-button')
-let removePopup = function () {
-  popup.classList.remove('popup_opened', 'popup-place_opened');
-}
+editButton.addEventListener('click', addPopup);
 closeButton.addEventListener('click', removePopup);
